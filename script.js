ebe954df2f20eb501afd9bafe82b779692ee5934
unknown <byampols@alumni.cmu.edu>
2021-09-21 19:30:36 -0400

    formatted moments

--- script.js
+++ script.js
@@ -1,26 +1,26 @@
-var currentDate= moment().format("MMM Do YY");
+var currentDate= moment();
 var forwardBtn = document.getElementById('forwardDate');
 var backBtn = document.getElementById('backDate');
 var todayBtn = document.getElementById('resetDate');
 var saveBtns = document.querySelectorAll('button[class^=saveBtn]');
 var number = 0;
 var dateContainer = document.getElementById('date');
-dateContainer.textContent = currentDate
+dateContainer.textContent = currentDate.format("MMM Do YYYY");
 console.log(saveBtns.length)
 function addToDate(){
 	number++
 	dateContainer.textContent= '';
 	
-	var add = moment(currentDate, "MMM Do YY").add(number,'days');
-	dateContainer.textContent =  add
+	var add = moment(currentDate).add(number,'days');
+	dateContainer.textContent =  add.format("MMM Do YYYY");
 }
 
 function subFromDate(){
 	number--;
 	dateContainer.textContent= '';
 	
-	var subtract = moment(currentDate, "MMM Do YY").add(number,'days');
-	dateContainer.textContent =  subtract;
+	var subtract = moment(currentDate).add(number,'days');
+	dateContainer.textContent =  subtract.format("MMM Do YYYY");
 
 }
 
@@ -29,8 +29,8 @@ function todayDate(){
 
 	dateContainer.textContent= '';
 	
-	var subtract = moment(currentDate, "MMM Do YY").add(number,'days');
-	dateContainer.textContent =  subtract;
+	var subtract = moment(currentDate).add(number,'days');
+	dateContainer.textContent =  subtract.format("MMM Do YYYY");
 }
 
 function save(){
