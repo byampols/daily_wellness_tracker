83ca127329eeb8ba07f51ec4247a68096f40e63d
unknown <byampols@alumni.cmu.edu>
2021-09-22 19:05:49 -0400

    completed dynamic generation for all elements

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,26 @@
 //GLOBAL VARIABLES//
 
 //query selectors
-var dataContainerEl = document.querySelector("main"); //replace main with proper location
+var dataContainerEl = document.querySelector("#time-blocks");
+var dateContainer = document.getElementById('date');
+var dailyScheduleEl = document.getElementById('daily-schedule');
+var upperDateEl = dailyScheduleEl.querySelector('p');
+var mealsEl = document.querySelector("input[name='Time-001']");
+var workoutEl = document.querySelector("input[name='Time-002']");
+var restEl = document.querySelector("input[name='Time-003']");
+
+//buttons for changing date
+var forwardBtn = document.getElementById('forwardDate');
+var backBtn = document.getElementById('backDate');
+var todayBtn = document.getElementById('resetDate');
+
+//buttons for saving and resetting
+var submitTop = document.getElementById("submit-top");
+var submitBottom = document.getElementById("submit-bottom");
+var resetTop = document.getElementById("reset-top");
+var resetBottom = document.getElementById("reset-bottom");
+
+//locations for getting weather
 var searchedLocationEl = document.querySelector("#searched-location");
 var locationContainerEl = document.querySelector("#location-container");
 var weather = document.getElementById("weather");
@@ -12,7 +31,7 @@ var endTime = 24;
 
 var defaultList = function() {
     var list = [];
-    var timeSlot = {meals: "", calories: "", workout: "", burn: "", note: ""};
+    var timeSlot = "";
     if (startTime > endTime) {
         return;
     }
@@ -22,23 +41,25 @@ var defaultList = function() {
     return list;
 };
 
+//initializes current date to today
+var currentDate = moment();
+dateContainer.textContent = currentDate.format("MMM Do, YYYY");
+upperDateEl.textContent = currentDate.format("MMM Do, YYYY");
+
+//format for creating the data object
+var storageFormat = "D/M/YYYY";
+
 //sets a default data for use later (DO NOT MODIFY)
-var defaultData = {sunday: defaultList(), monday: defaultList(), tuesday: defaultList(), wednesday: defaultList(), thursday: defaultList(), friday: defaultList(), saturday: defaultList()};
+var defaultDate = currentDate.format(storageFormat);
+var defaultData = {}; 
+defaultData[defaultDate] = {timeSlots: defaultList(), meals: "", workout: "", rest: ""};
 
 //current dataset (MODIFY THIS)
 var data = defaultData;
 
-var currentDate= moment();
-var forwardBtn = document.getElementById('forwardDate');
-var backBtn = document.getElementById('backDate');
-var todayBtn = document.getElementById('resetDate');
-var saveBtns = document.querySelectorAll('button[class^=saveBtn]');
-var number = 0;
-var dateContainer = document.getElementById('date');
-dateContainer.textContent = currentDate.format("MMM Do YYYY");
-var date = moment().format("dddd").toLowerCase();
-
 //FUNCTIONS//
+
+//removes nulls from data in any edgecases
 var denullify = function(data) {
     if (data === null) {
         return "";
@@ -92,122 +113,179 @@ function getWeather() {
       });
   };
 
+//saves current dataset to localstorage
 var saveData = function() {
     if (data === null) {
         data = defaultData;
     }
     localStorage.setItem("data",JSON.stringify(data));
     loadTimesChart();
-}
+};
 
+//loads data from local storage and returns the most recent saved dataset
 var loadData = function() {
     data = JSON.parse(localStorage.getItem("data"));
     if (data === null) {
         data = defaultData;
     }
     return data;
-}
+};
 
+//loads the timeslots for currentDate
 var loadTimesChart = function() {
     //set the current dataset
     data = loadData();
-    var dataToday = data[date];
+    var date = currentDate.format(storageFormat);
+
+    //if there is no data for the current date, create a new date in the data
+    if(!data[date]) {
+        data[date] = {timeSlots: defaultList(), meals: "", workout: "", rest: ""};
+    }
+
+    var dataToday = data[date].timeSlots;
+
+    //reset html
+    dataContainerEl.innerHTML = "";
     //set the starting time
     var currentTime = moment().day(date).hour(6).minute(0).second(0);
-    //.format("h:mm a")
+    //loop through length and create timeslots
     for (i = 0; i < dataToday.length; i++) {
         //acquire data to display
-        var time = currentTime.format("h:mm a");
-        var meals = denullify(dataToday[i].meals);
-        var calories = denullify(dataToday[i].calories);
-        var workout = denullify(dataToday[i].workout);
-        var burn = denullify(dataToday[i].burn);
-        var note = denullify(dataToday[i].note);
+        var time = currentTime.format("h:mma");
+        var text = denullify(dataToday[i]);
 
         //create elements
         var timeSlot = document.createElement("div");
         var timeBox = document.createElement("div");
-        var mealsBox = document.createElement("div");
-        var caloriesBox = document.createElement("div");
-        var workoutBox = document.createElement("div");
-        var burnBox = document.createElement("div");
-        var noteBox = document.createElement("div");
+        var textBox = document.createElement("textarea");
 
-        //class elements (ADD CLASSES)
-        timeSlot.classList = "";
-        timeBox.classList = "";
-        mealsBox.classList = "";
-        caloriesBox.classList = "";
-        workoutBox.classList = "";
-        burnBox.classList = "";
-        noteBox.classList = "";
+        //id timeSlot
+        timeSlot.id = `time-${i}`;
+
+        //class elements
+        timeSlot.classList = "row";
+        timeBox.classList = "col-sm-1 hour";
+        textBox.classList = "col-sm-11";
 
         //add text to elements
         timeBox.textContent = time;
-        mealsBox.textContent = meals;
-        caloriesBox.textContent = calories;
-        workoutBox.textContent = workout;
-        burnBox.textContent = burn;
-        noteBox.textContent = note;
+        textBox.textContent = text;
 
         //append elements
         timeSlot.appendChild(timeBox);
-        timeSlot.appendChild(mealsBox);
-        timeSlot.appendChild(caloriesBox);
-        timeSlot.appendChild(workoutBox);
-        timeSlot.appendChild(burnBox);
-        timeSlot.appendChild(noteBox);
+        timeSlot.appendChild(textBox);
         dataContainerEl.appendChild(timeSlot);
         //add 30 minutes to the time
         currentTime.add(30,'m');
     }
 };
+
+//loads today's plan
+var loadTodaysPlan = function() {
+    //set the current dataset
+    data = loadData();
+    var date = currentDate.format(storageFormat);
+
+    //if there is no data for the current date, create a new date in the data
+    if(!data[date]) {
+        data[date] = {timeSlots: defaultList(), meals: "", workout: "", rest: ""};
+    }
+
+    //set the value for the meals, workout, and rest
+    mealsEl.value = data[date].meals;
+    workoutEl.value = data[date].workout;
+    restEl.value = data[date].rest;
+};
 //EVENT HANDLERS//
 
+//adds 1 day to currentDate
 function addToDate(){
-    number++
     dateContainer.textContent= '';
     
-    var add = moment(currentDate).add(number,'days');
-	dateContainer.textContent =  add.format("MMM Do YYYY");
+    currentDate = moment(currentDate).add(1,'days');
+	dateContainer.textContent =  currentDate.format("MMM Do, YYYY");
+    upperDateEl.textContent = currentDate.format("MMM Do, YYYY");
+
+    loadTimesChart();
+    loadTodaysPlan();
 }
 
+//subtracts 1 day from currentDate
 function subFromDate(){
-    number--;
     dateContainer.textContent= '';
     
-    var subtract = moment(currentDate).add(number,'days');
-	dateContainer.textContent =  subtract.format("MMM Do YYYY");
+    currentDate = moment(currentDate).add(-1,'days');
+	dateContainer.textContent =  currentDate.format("MMM Do, YYYY");
+    upperDateEl.textContent = currentDate.format("MMM Do, YYYY");
 
+    loadTimesChart();
+    loadTodaysPlan();
 }
 
+//sets currentDate to today
 function todayDate(){
-    number = 0;
-
     dateContainer.textContent= '';
     
-    var subtract = moment(currentDate).add(number,'days');
-	dateContainer.textContent =  subtract.format("MMM Do YYYY");
+    currentDate = moment();
+	dateContainer.textContent =  currentDate.format("MMM Do, YYYY");
+    upperDateEl.textContent = currentDate.format("MMM Do, YYYY");
+
+    loadTimesChart();
+    loadTodaysPlan();
 }
 
-function save(){
-console.log('saved!')
+//saves all current data to data object, then saves to local storage
+var save = function() {
+    var date = currentDate.format(storageFormat);
+    data = loadData();
+    
+    //if there is no data for the current date, create a new date in the data
+    if(!data[date]) {
+        data[date] = {timeSlots: defaultList(), meals: "", workout: "", rest: ""};
+    }
 
-}
+    data[date].meals = mealsEl.value;
+    data[date].workout = workoutEl.value;
+    data[date].rest = restEl.value;
+
+    for (i = 0; i < data[date].timeSlots.length; i++) {
+        var timeBlockEl = document.getElementById(`time-${i}`);
+        var textAreaEl = timeBlockEl.querySelector("textarea");
+        var text = textAreaEl.value.trim();
+        data[date].timeSlots[i] = text;
+    }
+    saveData();
+};
+
+//resets currentDate's data set
+var reset = function() {
+    var date = currentDate.format(storageFormat);
+    data = loadData();
+    
+    //if there is no data for the current date, create a new date in the data
+    if(!data[date]) {
+        data[date] = {timeSlots: defaultList(), meals: "", workout: "", rest: ""};
+    }
+
+    data[date] = {timeSlots: defaultList(), meals: "", workout: "", rest: ""};
+    saveData();
+    loadTimesChart();
+    loadTodaysPlan();
+};
+
 //INITIAL FUNCTION CALLS//
-
-//loadTimesChart();
+loadTimesChart();
+loadTodaysPlan();
 
 //EVENT LISTNERS//
 forwardBtn.onclick = addToDate;
 backBtn.onclick = subFromDate;
 todayBtn.onclick= todayDate;
 
-for (let i = 0; i < saveBtns.length; i++) {
-    
-    saveBtns[i].addEventListener('click', save)
-    
-}
+submitTop.addEventListener('click', save);
+submitBottom.addEventListener('click', save);
+resetTop.addEventListener('click', reset);
+resetBottom.addEventListener('click', reset);
 //TIMED FUNCTION CALLS//
 
 
